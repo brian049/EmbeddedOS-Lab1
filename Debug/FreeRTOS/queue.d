b583c18d05e0dd611bf81ed0f999b717f9c5fb40
brian049 <brianshih1203@gmail.com>
2025-03-20 18:37:25 +0800

    Upload lab_1

---
+++ Debug/FreeRTOS/queue.d
@@ -0,0 +1,23 @@
+FreeRTOS/queue.o: ../FreeRTOS/queue.c \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/FreeRTOS.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/FreeRTOSConfig.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/projdefs.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/portable.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/deprecated_definitions.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/mpu_wrappers.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/task.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/list.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/queue.h \
+ /home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/task.h
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/FreeRTOS.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/FreeRTOSConfig.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/projdefs.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/portable.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/deprecated_definitions.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/mpu_wrappers.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/task.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/list.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/queue.h:
+/home/ubuntu22/STM32CubeIDE/workspace_1.17.0/lab_1/FreeRTOS/include/task.h:
